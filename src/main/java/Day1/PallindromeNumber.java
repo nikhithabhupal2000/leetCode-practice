d89836015a5b1fffb63031aba80f83c1125571bf
Gita Mittala <gita.mittala@nordstrom.com>
2020-03-14 20:49:20 -0700

    resolve merge  conflicts

--- src/main/java/Day1/PallindromeNumber.java
+++ src/main/java/Day1/PallindromeNumber.java
@@ -1,4 +1,4 @@
-/*
+package Day1;/*
 Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
 
 Coud you solve it without converting the integer to a string
--- src/main/java/Day2/ValidParanthesis.java
+++ src/main/java/Day2/ValidParanthesis.java
@@ -1,4 +1,4 @@
-/*
+package Day2;/*
  *
  *Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
  *
